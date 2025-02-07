87cd687c04d33f7026c380da9c28a4216ee9b9e5
fcwe1113 <fcwe1113@gmail.com>
2025-02-07 13:53:45 +0000

    indenting

--- src/client_connection.rs
+++ src/client_connection.rs
@@ -59,13 +59,13 @@ pub(crate) async fn client_connection(stream: TcpStream, addr: SocketAddr, token
                 // messages sent from both ends should follow a similar format (at least for the first few chars)
                 // *** denotes client side tasks
                 // 0 = token exchange
-                // a. when the websocket channel opens the server will generate the token and send it to the client, the client would have a placeholder token which prevent the client from proceeding
-                // b. the client saves the token and pings back the same token to the server***
-                // c. the server sends an ack back if the token matches and switches the token_exchanged flag on and saves it in the list(tm)
-                // d. client then tells the server to move on to the start screen state***
-                // e. server tells client to move on then moves on itself, unless the token_exchanged flag is not on, in which case handle the error
-                // f. client moves on for real***
-                // while the client is waiting for the token exchange ack it can show a loading wheel or something idk
+                    // a. when the websocket channel opens the server will generate the token and send it to the client, the client would have a placeholder token which prevent the client from proceeding
+                    // b. the client saves the token and pings back the same token to the server***
+                    // c. the server sends an ack back if the token matches and switches the token_exchanged flag on and saves it in the list(tm)
+                    // d. client then tells the server to move on to the start screen state***
+                    // e. server tells client to move on then moves on itself, unless the token_exchanged flag is not on, in which case handle the error
+                    // f. client moves on for real***
+                    // while the client is waiting for the token exchange ack it can show a loading wheel or something idk
                 // 1 = start screen
                 // 2 = sign up screen
                 // 3 = store locator
