3e529c4080fc431b67f5b0b5dcc10a746a50087e
fcwe1113 <fcwe1113@gmail.com>
2025-03-25 18:27:50 +0000

    main app done except checkout

--- src/main_app.rs
+++ src/main_app.rs
@@ -21,13 +21,14 @@ use crate::screen_state::ScreenState;
 struct ItemInfo {
     id: i32,
     name: String,
-    price: i32,
+    price: f64,
     age_limit: i32,
+    divisible: bool,
 }
 
 impl ItemInfo {
-    fn new(id: i32, name: String, price: i32, age_limit: i32) -> Self {
-        ItemInfo { id, name, price, age_limit }
+    fn new(id: i32, name: String, price: f64, age_limit: i32, divisible: bool) -> Self {
+        ItemInfo { id, name, price, age_limit, divisible }
     }
 }
 
@@ -54,7 +55,8 @@ pub(crate) async fn main_app_handler(
             // note the server WILL NOT be keeping track of the shopping list in this stage
     // d. client takes in the json and display the item on a list on screen
     // e. when user clicks the checkout button client sends the entire list of items as a json with quantity up to server like "4CHECKOUT[json]"
-    // f. move on to payment
+    // f. server sends an ACK
+    // g. move on to payment
 
     let result = main_app_screen(msg, sender, addr, token, nonce, status_check_timer, shop_id, username, db);
     if let Err(err) = resolve_result(result, addr, list_lock.clone()).await {
@@ -83,52 +85,59 @@ async fn main_app_screen(
             // resets the timer when the status check is received
             debug!("status checked for {}", addr);
             *status_check_timer = 0;
-            match update_nonce(nonce, sender, addr).await{
+            match update_nonce(nonce, sender, addr).await {
                 Ok(s) => { Ok(s) }
                 Err(e) => { bail!(e); }
             }
-        } else if msg.chars().take(4).collect::<String>() == "ITEM" {
-
-            // technically trying to find a number id with text wont break the system
-            // but i dont wanna come fix this if someone typed in the magic hacking words that can somehow take the system down
-            let id = match msg.chars().skip(4).collect::<String>().parse::<i32>() {
-                Ok(i) => i,
-                Err(e) => {
-                    sender.send(Message::from("4INVALID")).await?;
-                    bail!("client {} sent invalid item id: {}", addr, e);
-                }
-            };
-
-            let result = task::block_in_place(|| { // 2f. check username against db
-                let mut stmt = db.prepare("SELECT id, name, price_per_unit, age_limit FROM Items WHERE id = ?").unwrap();
-                let query_iter = stmt.query_map([&id], |row| {
-                    Ok(ItemInfo {
-                        id: row.get(0)?,
-                        name: row.get(1)?,
-                        price: row.get(2)?,
-                        age_limit: match row.get(3) {
-                            Ok(i) => match i {
-                                None => 0,
-                                _ => i.expect("lol"), // todo may not be functional
-                            },
-                            _ => {0}
-                        },
-                    })
-                }).unwrap();
-                return query_iter.collect::<Result<Vec<_>, _>>().unwrap()
-            });
-
-            if result.is_empty() { // if the vector is empty that means no item of that id was found
-                sender.send(Message::from("4INVALID")).await?;
-            } else {
-                // if the query result is not empty that means it suceeded
-                sender.send(Message::from(format!("4ITEM{}", serde_json::to_string(&result[0]).unwrap()))).await?;
-            }
-
-            Ok("STATUS ok".to_string())
         } else {
             bail!("invalid status check for {}", addr);
         }
+    } else if msg.chars().take(4).collect::<String>() == "ITEM" {
+
+        // technically trying to find a number id with text wont break the system
+        // but i dont wanna come fix this if someone typed in the magic hacking words that can somehow take the system down
+        let id = match msg.chars().skip(4).collect::<String>().parse::<i32>() {
+            Ok(i) => i,
+            Err(e) => {
+                sender.send(Message::from("4INVALID")).await?;
+                error!("client {} sent invalid item id: {}", addr, e);
+                return Ok("STATUS ok".to_string())
+            }
+        };
+
+        let result = task::block_in_place(|| { // 2f. check username against db
+            let mut stmt = db.prepare("SELECT id, name, price_per_unit, age_limit, unit FROM Items WHERE id = ?").unwrap();
+            let query_iter = stmt.query_map([&id], |row| {
+                Ok(ItemInfo {
+                    id: row.get(0)?,
+                    name: row.get(1)?,
+                    price: row.get(2)?,
+                    age_limit: match row.get(3) {
+                        Ok(i) => match i {
+                            None => 0,
+                            _ => i.expect("lol"), // todo may not be functional
+                        },
+                        _ => { 0 }
+                    },
+                    divisible: match row.get::<usize, String>(4) {
+                        Ok(_) => true,
+                        _ => false
+                    },
+                })
+            }).unwrap();
+            return query_iter.collect::<Result<Vec<_>, _>>().unwrap()
+        });
+
+        if result.is_empty() { // if the vector is empty that means no item of that id was found
+            sender.send(Message::from("4INVALID")).await?;
+            debug!("sent item id invalid message to client {}", addr);
+        } else {
+            // if the query result is not empty that means it suceeded
+            sender.send(Message::from(format!("4ITEM{}", serde_json::to_string(&result[0]).unwrap()))).await?;
+            debug!("sent data for item id {} to client {}", id, addr);
+        }
+
+        Ok("STATUS ok".to_string())
     } else {
         bail!("main app screen received invalid message from {}: {}", addr, msg);
     }
