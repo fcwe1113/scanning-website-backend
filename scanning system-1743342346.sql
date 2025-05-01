c2eed4a711373966b7d7e5924269d441004248cb
fcwe1113 <fcwe1113@gmail.com>
2025-05-01 13:34:45 +0100

    redid the sql schema file

--- scanning system-1743342346.sql
+++ scanning system-1743342346.sql
@@ -1,46 +1,64 @@
-CREATE TABLE IF NOT EXISTS `Items` (
-	`id` integer primary key NOT NULL UNIQUE,
-	`name` TEXT NOT NULL,
-	`price_per_unit` REAL NOT NULL,
-	`unit` TEXT,
-	`age_limit` INTEGER
+-- Users definition
+
+CREATE TABLE Users (
+                       username TEXT NOT NULL,
+                       password TEXT NOT NULL,
+                       first_name TEXT NOT NULL,
+                       last_name TEXT NOT NULL,
+                       dob TEXT NOT NULL,
+                       email TEXT NOT NULL,
+                       card_num TEXT,
+                       "exp" TEXT,
+                       cvv TEXT,
+                       CONSTRAINT Users_PK PRIMARY KEY (username)
 );
-CREATE TABLE IF NOT EXISTS `Shops` (
-	`id` integer primary key NOT NULL UNIQUE,
-	`name` TEXT NOT NULL,
-	`address` TEXT NOT NULL UNIQUE
+
+-- Shops definition
+
+CREATE TABLE `Shops` (
+                         `id` integer primary key NOT NULL UNIQUE,
+                         `name` TEXT NOT NULL,
+                         `address` TEXT NOT NULL UNIQUE
 );
-CREATE TABLE IF NOT EXISTS `Shop_stock` (
-	`shop_id` INTEGER NOT NULL,
-	`item_id` INTEGER NOT NULL,
-	`stock` INTEGER,
-FOREIGN KEY(`shop_id`) REFERENCES `Shops`(`id`),
-FOREIGN KEY(`item_id`) REFERENCES `Items`(`id`)
+
+-- Items definition
+
+CREATE TABLE `Items` (
+                         `id` integer primary key NOT NULL UNIQUE,
+                         `name` TEXT NOT NULL,
+                         `price_per_unit` INTEGER,
+                         `unit` TEXT,
+                         `age_limit` INTEGER
 );
-CREATE TABLE IF NOT EXISTS `Users` (
-	`username` TEXT NOT NULL UNIQUE,
-	`password` TEXT NOT NULL,
-	`salt` TEXT NOT NULL UNIQUE,
-	`first_name` TEXT NOT NULL,
-	`last_name` TEXT NOT NULL,
-	`dob` REAL NOT NULL,
-	`email` TEXT NOT NULL,
-	`card_num` TEXT,
-	`exp` REAL,
-	`cvv` TEXT
+
+-- Transactions definition
+
+CREATE TABLE Transactions (
+                              id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                              username TEXT NOT NULL,
+                              shop_id INTEGER NOT NULL,
+                              time TEXT NOT NULL,
+                              CONSTRAINT Transactions_Shops_FK FOREIGN KEY (shop_id) REFERENCES Shops(id),
+                              CONSTRAINT Transactions_Users_FK FOREIGN KEY (username) REFERENCES Users(username)
 );
-CREATE TABLE IF NOT EXISTS `Transactions` (
-	`id` INTEGER NOT NULL,
-	`username` INTEGER NOT NULL,
-	`shop_id` INTEGER NOT NULL,
-	`time` REAL NOT NULL,
-FOREIGN KEY(`username`) REFERENCES `Users`(`username`),
-FOREIGN KEY(`shop_id`) REFERENCES `Shops`(`id`)
+
+-- Purchase_records definition
+
+CREATE TABLE Purchase_records (
+                                  transaction_id INTEGER NOT NULL,
+                                  item_id INTEGER NOT NULL,
+                                  quantity REAL NOT NULL,
+                                  CONSTRAINT Purchase_records_PK PRIMARY KEY (transaction_id,item_id),
+                                  CONSTRAINT Purchase_records_Items_FK FOREIGN KEY (item_id) REFERENCES Items(id),
+                                  CONSTRAINT Purchase_records_Transactions_FK FOREIGN KEY (transaction_id) REFERENCES Transactions(id)
 );
-CREATE TABLE IF NOT EXISTS `Purchase_records` (
-	`transaction_id` INTEGER NOT NULL,
-	`item_id` INTEGER NOT NULL,
-	`quantity` REAL NOT NULL,
-FOREIGN KEY(`transaction_id`) REFERENCES `Transactions`(`id`),
-FOREIGN KEY(`item_id`) REFERENCES `Items`(`id`)
+
+-- Shop_stock definition
+
+CREATE TABLE `Shop_stock` (
+                              `shop_id` INTEGER NOT NULL,
+                              `item_id` INTEGER NOT NULL,
+                              `stock` INTEGER,
+                              FOREIGN KEY(`shop_id`) REFERENCES `Shops`(`id`),
+                              FOREIGN KEY(`item_id`) REFERENCES `Items`(`id`)
 );
